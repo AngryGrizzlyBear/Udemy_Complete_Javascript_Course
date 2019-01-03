167a091f2944314b4f3c5ef1fce25c60a57100ec
Evan <evanmlongwood@gmail.com>
2019-01-03 14:08:54 -0800

    Finished up booleans. Moving on to advanced booleans.

--- JS_Boot_Camp/basics/boolean.js
+++ JS_Boot_Camp/basics/boolean.js
@@ -5,24 +5,43 @@
 // >= greater than or equal to
 // <= less than or equal to
 
-let temp = 31;
-let isFreezing = temp < 32;
+let temp = 89;
 
-console.log(isFreezing);
+if (temp <=32) {
+    console.log('It is freezing outside!')
+} else {
+    if(temp >= 110) {
+        console.log('It is not freezing....It is waay too hot outside!')
+    } else {
+        console.log("It's not so bad!")
+    }
+}
 
 // Challenge
 
 // Create age set to your age
-let age = 30;
+let age = 67;
 
 // Calculate is child - if they are 7 or under
-let isChild = age <= 7;
+// let isChild = age <= 7;
 
 // Calculate is senior - if they are 65 or older
-let isSenior = age >= 65;
+// let isSenior = age >= 65;
 
 // print is child value
-console.log(isChild);
+// console.log(isChild);
 
 // print is senior value
-console.log(isSenior);
+// console.log(isSenior);
+
+// if 7 or under print message about child pricing.
+if (age <= 7) {
+    console.log("You're getting a children's discount!")
+} else {
+    if(age >= 65) {
+        console.log("You're getting the senior's discount!")
+    } else {
+        console.log("You have to pay the normal price, bub.")
+    }
+}
+// If 65 or Older print message about senior discount
