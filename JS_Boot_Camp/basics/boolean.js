d9a239b8cd029957d4b2f8d1e0e66f3963cad1b0
Evan <evanmlongwood@gmail.com>
2019-01-03 13:23:43 -0800

    Booleans with challenge.

---
+++ JS_Boot_Camp/basics/boolean.js
@@ -0,0 +1,28 @@
+// === - equality operator
+// !== - no equality operator
+// < - less than operator
+// > - greater than operator
+// >= greater than or equal to
+// <= less than or equal to
+
+let temp = 31;
+let isFreezing = temp < 32;
+
+console.log(isFreezing);
+
+// Challenge
+
+// Create age set to your age
+let age = 30;
+
+// Calculate is child - if they are 7 or under
+let isChild = age <= 7;
+
+// Calculate is senior - if they are 65 or older
+let isSenior = age >= 65;
+
+// print is child value
+console.log(isChild);
+
+// print is senior value
+console.log(isSenior);
