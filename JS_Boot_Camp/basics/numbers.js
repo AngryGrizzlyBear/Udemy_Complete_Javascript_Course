2b97ca4eae7c5085964701db39251aa4f1f84e64
Evan <evanmlongwood@gmail.com>
2019-01-03 12:26:57 -0800

    Variables. Basics.

---
+++ JS_Boot_Camp/basics/numbers.js
@@ -0,0 +1,18 @@
+let age = 26
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears);
+
+// Challenge Area
+// student score 18
+
+let studentScore = 18;
+
+// MaxScore 20
+let maxScore = 20;
+
+// percent variable
+percentTotal = (studentScore / maxScore) * 100 + '%';
+
+// print
+console.log(percentTotal);
