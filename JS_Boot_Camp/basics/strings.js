3254dd3aa7134524814c8a07a691bae5aec4366d
Evan <evanmlongwood@gmail.com>
2019-01-03 12:09:15 -0800

    Strings. Continuation of the basics.

---
+++ JS_Boot_Camp/basics/strings.js
@@ -0,0 +1,22 @@
+let name = 'Andrew Mead';
+let firstName = 'Andrew';
+let lastName = 'Mead';
+let fullName = firstName + ' ' + lastName;
+
+console.log(name);
+console.log(firstName);
+console.log(lastName);
+console.log(firstName + ' ' + lastName);
+console.log(fullName);
+
+
+// Challenge
+// Make three variables, city , country, location.
+// location is a combination of the two.
+
+let city = 'Philadelphia';
+let country = 'United States';
+let location = city + ', ' + country;
+
+console.log(location);
+
