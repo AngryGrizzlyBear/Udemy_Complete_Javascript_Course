00a8d009b4d1796a726a34a87fd8e533024e1956
Evan <evanmlongwood@gmail.com>
2019-01-22 15:39:18 -0800

    Arguments.

---
+++ JS_Boot_Camp/functions/arguments.js
@@ -0,0 +1,31 @@
+// Multiple Arguments
+let add = function (a, b, c) {
+ return a + b + c
+};
+
+let result = add(10, 1, 13);
+console.log(result);
+
+// Default arguments
+let getScoreText = function (name = 'Anonymous', score = 0) {
+    return 'Name: ' + name + ' - Score: ' + score
+};
+
+let scoreText = getScoreText(undefined, 99);
+console.log(scoreText);
+
+// Challenge area
+// total, tipPercent, .1 .2 .3 (100 x .1 .2 or .5)
+
+let getTip = function (total, tipPercent= .2) {
+   return total * tipPercent
+};
+
+total = getTip(412.22,);
+total2 = getTip(23.42, .1);
+total3 = getTip(70.23, .3);
+total4 = getTip(100)
+console.log(total);
+console.log(total2);
+console.log(total3);
+console.log(total4);
