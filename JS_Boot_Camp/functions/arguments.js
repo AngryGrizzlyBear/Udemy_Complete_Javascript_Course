4e5aa5c46934d0abcceb3aa463b68981f4160bac
Evan <evanmlongwood@gmail.com>
2019-01-23 10:47:45 -0800

    Strings

--- JS_Boot_Camp/functions/arguments.js
+++ JS_Boot_Camp/functions/arguments.js
@@ -8,7 +8,8 @@ console.log(result);
 
 // Default arguments
 let getScoreText = function (name = 'Anonymous', score = 0) {
-    return 'Name: ' + name + ' - Score: ' + score
+    return `Name: ${name} - Score: ${score}`
+    // return 'Name: ' + name + ' - Score: ' + score
 };
 
 let scoreText = getScoreText(undefined, 99);
@@ -24,8 +25,12 @@ let getTip = function (total, tipPercent= .2) {
 total = getTip(412.22,);
 total2 = getTip(23.42, .1);
 total3 = getTip(70.23, .3);
-total4 = getTip(100)
+total4 = getTip(100);
 console.log(total);
 console.log(total2);
 console.log(total3);
 console.log(total4);
+
+let name = 'Vikram';
+let age = 24;
+console.log(`Hey, my name is ${name}! I am ${age} years old.`);
