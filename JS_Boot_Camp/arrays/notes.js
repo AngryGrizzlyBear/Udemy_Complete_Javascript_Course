1865257b1c8085526885cbca9aa89f5c8b3798c1
Evan <evanmlongwood@gmail.com>
2019-01-29 16:51:14 -0800

    Methods and Arrays.

--- JS_Boot_Camp/arrays/notes.js
+++ JS_Boot_Camp/arrays/notes.js
@@ -1,6 +1,16 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
 // MDN array
+//
+// console.log(notes.pop());
+// notes.push('My new note.');
+//
+// console.log(notes.shift());
+// notes.unshift('My First Note');
+
+// notes.splice(1, 1, 'This is the new second item');
+
+notes[2] = 'This is now the new note 3.';
 
 console.log(notes.length);
-console.log(notes[notes.length - 2]);
+console.log(notes);
--- JS_Boot_Camp/arrays/todo.js
+++ JS_Boot_Camp/arrays/todo.js
@@ -3,5 +3,15 @@
 // print the first and second to last items -> Todo: Walk the dog
 
 const todo = ['Walk the dog', 'Go to the store', 'Buy Lunch', 'See your Boss', 'Sleep'];
-console.log(`You have ${todo.length} things to do.`);
-console.log(`Todo: ${todo[0]} and ${todo[todo.length -2]}`);
+// console.log(`You have ${todo.length} things to do.`);
+// console.log(`Todo: ${todo[0]} and ${todo[todo.length -2]}`);
+
+// Delete the 3rd item
+// add a new item on the end.
+// Remove the first item from the list.
+todo.splice(2, 1);
+// You can also push to add something to the end. Splice works too.
+todo.splice(4, 0, 'Say Hi to co-workers');
+// Shift also gets rid of the first one. Splice or shift works.
+todo.splice(0, 1);
+console.log(todo);
