702775d7b969a25ad03af0826043c07a24055bfc
Evan <evanmlongwood@gmail.com>
2019-01-29 16:22:41 -0800

    Arrays.

---
+++ JS_Boot_Camp/arrays/notes.js
@@ -0,0 +1,6 @@
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+
+// MDN array
+
+console.log(notes.length);
+console.log(notes[notes.length - 2]);
---
+++ JS_Boot_Camp/arrays/todo.js
@@ -0,0 +1,7 @@
+// Create an array with five different todos
+// You have x todos.
+// print the first and second to last items -> Todo: Walk the dog
+
+const todo = ['Walk the dog', 'Go to the store', 'Buy Lunch', 'See your Boss', 'Sleep'];
+console.log(`You have ${todo.length} things to do.`);
+console.log(`Todo: ${todo[0]} and ${todo[todo.length -2]}`);
