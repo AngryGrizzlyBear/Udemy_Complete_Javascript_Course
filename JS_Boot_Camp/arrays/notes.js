f9625551532a2882855cfdefa881174352b43b54
Evan <evanmlongwood@gmail.com>
2019-01-30 16:35:28 -0800

    Searching through Arrays, part 2 with challenge. (This was a little difficult.)

--- JS_Boot_Camp/arrays/notes.js
+++ JS_Boot_Camp/arrays/notes.js
@@ -1,4 +1,45 @@
-const notes = ['Note 1', 'Note 2', 'Note 3'];
+const notestwo = [ {
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}];
+
+const findNote = function (notes, noteTitle) {
+    return notes.find(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    });
+    // const note = notes.find(function (note, index) {
+    //     return note.title.toLowerCase() === noteTitle.toLowerCase();
+    // });
+    return note
+};
+
+// const findNote = function (notes, noteTitle) {
+//     const index = notes.findIndex(function (note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase();
+//     });
+//     return notes[index]
+// };
+
+const note = findNote(notestwo, 'Office modification');
+console.log(note);
+
+// console.log(notestwo.length);
+// console.log(notestwo);
+
+// // MDN findIndex()
+// const index = notestwo.findIndex(function (note, index) {
+//     console.log(note);
+//     return note.title === 'Habbits to work on'
+// });
+// console.log(index);
+
+// const notes = ['Note 1', 'Note 2', 'Note 3'];
 
 // MDN array
 //
@@ -10,27 +51,30 @@ const notes = ['Note 1', 'Note 2', 'Note 3'];
 
 // notes.splice(1, 1, 'This is the new second item');
 
-notes[2] = 'This is now the new note 3.';
+// notes[2] = 'This is now the new note 3.';
 
 // callback function
 // Also a loop
-notes.forEach(function (item, index) {
-   console.log(index);
-    console.log(item);
-});
-
-console.log(notes.length);
-console.log(notes);
+// notes.forEach(function (item, index) {
+//    console.log(index);
+//     console.log(item);
+// });
+//
+// console.log(notes.length);
+// console.log(notes);
 
 // Counting... 1
-for (let count = 2; count >= 0; count--) {
-    console.log(count)
-};
+// for (let count = 2; count >= 0; count--) {
+//     console.log(count)
+// };
+//
+// for (let count = 0; count < notes.length; count++) {
+//     console.log(notes[count])
+// }
+//
+// for (let count = notes.length - 1; count >= 0; count--){
+//     console.log(notes[count])
+// };
+//
+// console.log(notes.indexOf('Note 2'));
 
-for (let count = 0; count < notes.length; count++) {
-    console.log(notes[count])
-}
-
-for (let count = notes.length - 1; count >= 0; count--){
-    console.log(notes[count])
-};
--- JS_Boot_Camp/arrays/todo.js
+++ JS_Boot_Camp/arrays/todo.js
@@ -2,31 +2,64 @@
 // You have x todos.
 // print the first and second to last items -> Todo: Walk the dog
 
-const todo = ['Walk the dog', 'Go to the store', 'Buy Lunch', 'See your Boss', 'Sleep'];
+// const todo = ['Walk the dog', 'Go to the store', 'Buy Lunch', 'See your Boss', 'Sleep'];
 // console.log(`You have ${todo.length} things to do.`);
 // console.log(`Todo: ${todo[0]} and ${todo[todo.length -2]}`);
 
 // Delete the 3rd item
 // add a new item on the end.
 // Remove the first item from the list.
-todo.splice(2, 1);
-// You can also push to add something to the end. Splice works too.
-todo.splice(4, 0, 'Say Hi to co-workers');
-// Shift also gets rid of the first one. Splice or shift works.
-todo.splice(0, 1);
-
-//1. The first item
-//2. The second item
-todo.forEach( function (item, index) {
-    const num = index + 1;
-    console.log(`${num}. ${item}`)
-});
+// todo.splice(2, 1);
+// // You can also push to add something to the end. Splice works too.
+// todo.splice(4, 0, 'Say Hi to co-workers');
+// // Shift also gets rid of the first one. Splice or shift works.
+// todo.splice(0, 1);
+//
+// //1. The first item
+// //2. The second item
+// todo.forEach( function (item, index) {
+//     const num = index + 1;
+//     console.log(`${num}. ${item}`)
+// });
 
 // 1. Order cat food
 // 2. Clean kitchen.
 // ....
 
-for (let count = 0; count < todo.length; count++) {
-    const num = count + 1;
-    console.log(`${num}. ${todo[count]}`)
+// for (let count = 0; count < todo.length; count++) {
+//     const num = count + 1;
+//     console.log(`${num}. ${todo[count]}`)
+// };
+
+
+// 1. Convert array to array of objects -> text, completed
+// 1. Create a function to remove a todo by text value.
+const todo = [{
+    text: 'Walk the Dog',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: false
+}, {
+    text: 'Watch TV',
+    completed: false,
+}, {
+    text: 'Hug Wife',
+    completed: true,
+}, {
+    text: 'Call Dad',
+    completed: true,
+}
+];
+
+const deleteToDo = function (todos, todoText) {
+    const index = todos.findIndex(function (todo){
+        return todo.text.toLowerCase() === todoText.toLocaleLowerCase()
+    });
+    if (index > -1) {
+        todos.splice(index, 1)
+    }
 };
+
+deleteToDo(todo, 'Walk the Dog');
+console.log(todo);
