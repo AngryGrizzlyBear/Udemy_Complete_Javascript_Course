241f430ddb298d31106aa8807b5fa87f6f942b02
Evan <evanmlongwood@gmail.com>
2019-02-04 10:24:34 -0800

    Handling User Interaction.

--- JS_Boot_Camp/notes-app/notes-app.js
+++ JS_Boot_Camp/notes-app/notes-app.js
@@ -1,13 +1,58 @@
 // DOM-Document Object Model
+const notes = [ {
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}];
+
+document.querySelector('button').addEventListener('click', function (e) {
+    e.target.textContent = 'The button was clicked.'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Query and remove
 // const p = document.querySelector('p');
 // p.remove();
 
-const ps = document.querySelectorAll('p');
-
-ps.forEach(function (p) {
-    p.textContent = '********'
-    // console.log(p.textContent)
-    // p.remove()
-});
+// const ps = document.querySelectorAll('p');
+//
+// ps.forEach(function (p) {
+//     p.textContent = '********'
+//     // console.log(p.textContent)
+//     // p.remove()
+// });
+//
+// // Add a new element
+// const newParagraph = document.createElement('p');
+// newParagraph.textContent = 'This is a new element from Javascript';
+// document.querySelector('body').appendChild(newParagraph);
--- JS_Boot_Camp/todo-app/todo-app.js
+++ JS_Boot_Camp/todo-app/todo-app.js
@@ -1,8 +1,52 @@
-const paragraphs = document.querySelectorAll('p');
+const todos = [{
+    text: 'Walk the Dog',
+    completed: false,
+}, {
+    text: 'Buy food',
+    completed: false
+}, {
+    text: 'Watch TV',
+    completed: true,
+}, {
+    text: 'Hug Wife',
+    completed: true,
+}, {
+    text: 'Call Dad',
+    completed: true,
+}];
 
-paragraphs.forEach(function (p) {
-    if(p.textContent.includes('the')) {
-        p.remove()
-    }
-
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
 });
+
+const summary = document.createElement('h2');
+
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector('body').appendChild(summary);
+
+todos.forEach(function (todo) {
+   const p = document.createElement('p');
+   p.textContent = todo.text;
+   document.querySelector('body').appendChild(p)
+});
+
+// Listen for new todos creation
+
+document.querySelector('button').addEventListener('click', function (e) {
+    console.log('The button was clicked!')
+});
+
+
+
+// Challenge Area
+// You have 2 todos left (p element)
+// Add a p for each todos above (use text value)
+
+// const paragraphs = document.querySelectorAll('p');
+//
+// paragraphs.forEach(function (p) {
+//     if(p.textContent.includes('the')) {
+//         p.remove()
+//     }
+//
+// });
